88b31fbec9170d92de7dbfa373e7f9bc14416211
Aleksey Pirogov <astynax@users.noreply.github.com>
2019-02-28 22:32:23 +0300

    Migrate to elm 0.19

--- Main.elm
+++ Main.elm
@@ -1,7 +1,12 @@
 module Main exposing (main)
 
+import Browser
+import Collage exposing (..)
+import Collage.Events exposing (onClick)
+import Collage.Render exposing (svg)
+import Collage.Text as Text
 import Color
-import Graphics.Render exposing (..)
+import Debug exposing (toString)
 import Html exposing (Html)
 import Html.Events as Events
 import List
@@ -13,9 +18,8 @@ type alias List2d =
 
 
 type alias Config =
-    { rows : Int
-    , cols : Int
-    , size : Float
+    { cells : Int
+    , cellSize : Float
     }
 
 
@@ -36,14 +40,13 @@ type Msg
     | NewGame
 
 
-main : Program Never Model Msg
+main : Program () Model Msg
 main =
-    Html.program
+    Browser.element
         { init =
             init
-                { rows = 7
-                , cols = 7
-                , size = 600
+                { cells = 7
+                , cellSize = 80
                 }
         , view = view
         , update = update
@@ -51,15 +54,15 @@ main =
         }
 
 
-init : Config -> ( Model, Cmd Msg )
-init c =
-    { field = init2d c.rows c.cols
-    , moves = 0
-    , config = c
-    }
-        ! [ Random.generate GotClicks <|
-                genClicks c.rows c.cols
-          ]
+init : Config -> flags -> ( Model, Cmd Msg )
+init c _ =
+    ( { field = init2d c.cells c.cells
+      , moves = 0
+      , config = c
+      }
+    , genClicks c.cells c.cells
+        |> Random.generate GotClicks
+    )
 
 
 view : Model -> Html Msg
@@ -68,9 +71,12 @@ view model =
         [ Html.button [ Events.onClick NewGame ]
             [ Html.text "New game" ]
         , Html.text "Moves:"
-        , Html.text <| toString <| model.moves
+        , model.moves
+            |> toString
+            |> Html.text
         , if gameIsOver model.field then
-            renderGameOver model.config.size
+            renderGameOver (model.config.cellSize * toFloat model.config.cells)
+
           else
             render model
         ]
@@ -82,63 +88,59 @@ render model =
         rows =
             model.field
 
-        size =
-            model.config.size
-
         s =
-            size / toFloat (List.length rows)
+            model.config.cellSize
 
         viewRow y row =
-            position ( s / 2, s / 2 + toFloat y * s ) <|
-                group <|
-                    List.indexedMap
-                        (viewCell y)
-                        row
+            List.indexedMap (viewCell y) row
+                |> group
+                |> shift ( s / 2, s / 2 + toFloat y * s )
 
         viewCell y x st =
-            position ( toFloat x * s, 0 ) <|
-                onClick (Clicked <| Click x y) <|
-                    vault s st
+            vault s st
+                |> onClick (Clicked <| Click x y)
+                |> shift ( toFloat x * s, 0 )
     in
-        svg 0 0 size size <|
-            group <|
-                List.indexedMap viewRow rows
+    List.indexedMap viewRow rows
+        |> group
+        |> svg
 
 
 renderGameOver : Float -> Html a
 renderGameOver size =
-    svg 0 0 size size <|
-        position ( size / 2, size / 2 ) <|
-            group
-                [ filled (solid Color.orange) <|
-                    rectangle
-                        size
-                        size
-                , centered <|
-                    fontColor Color.yellow <|
-                        text 72 "You win!"
-                ]
+    group
+        [ Text.fromString "You win!"
+            |> Text.size 72
+            |> Text.color Color.yellow
+            |> rendered
+        , square size
+            |> filled (uniform Color.orange)
+        ]
+        |> shift ( size / 2, size / 2 )
+        |> svg
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Clicked c ->
-            { model
+            ( { model
                 | field = toggle2d c model.field
                 , moves = model.moves + 1
-            }
-                ! []
+              }
+            , Cmd.none
+            )
 
         GotClicks cs ->
-            { model
+            ( { model
                 | field = List.foldl toggle2d model.field cs
                 , moves = 0
-            }
-                ! []
+              }
+            , Cmd.none
+            )
 
         NewGame ->
-            init model.config
+            init model.config ()
 
 
 subscriptions : Model -> Sub Msg
@@ -146,43 +148,44 @@ subscriptions _ =
     Sub.none
 
 
-vault : Float -> Bool -> Form a
+vault : Float -> Bool -> Collage a
 vault size st =
     let
         bar w h =
             if st then
                 rectangle w h
+
             else
                 rectangle h w
 
         shadow =
             group
-                [ filled (solid Color.darkGray) <|
-                    bar (size / 10) (size * 0.8)
-                , filled (solid Color.darkGray) <|
-                    circle (size / 6)
+                [ bar (size / 10) (size * 0.8)
+                    |> filled (uniform Color.darkGray)
+                , circle (size / 6)
+                    |> filled (uniform Color.darkGray)
                 ]
 
         knob =
             group
-                [ filled (solid Color.black) <|
-                    bar (size / 10) (size * 0.8)
-                , filled (solid Color.darkGray) <|
-                    bar (size / 15) (size * 0.75)
-                , filled (solid Color.black) <|
-                    circle (size / 6)
-                , filled (solid Color.darkGray) <|
-                    circle (size / 7)
+                [ circle (size / 7)
+                    |> filled (uniform Color.darkGray)
+                , circle (size / 6)
+                    |> filled (uniform Color.black)
+                , bar (size / 15) (size * 0.75)
+                    |> filled (uniform Color.darkGray)
+                , bar (size / 10) (size * 0.8)
+                    |> filled (uniform Color.black)
                 ]
     in
-        position ( size / 2, size / 2 ) <|
-            group
-                [ filled (solid Color.gray) <|
-                    rectangle size size
-                , position ( size / 9, size / 9 ) <|
-                    shadow
-                , knob
-                ]
+    shift ( size / 2, size / 2 ) <|
+        group
+            [ knob
+            , shadow
+                |> shift ( size / 10, size / 10 )
+            , square size
+                |> filled (uniform Color.gray)
+            ]
 
 
 init2d : Int -> Int -> List2d
@@ -192,35 +195,35 @@ init2d w h =
 
 toggle2d : Click -> List2d -> List2d
 toggle2d (Click x y) =
-    List.indexedMap
-        (\yy ->
+    List.indexedMap <|
+        \yy ->
             if yy == y then
                 List.map not
+
             else
-                List.indexedMap
-                    (\xx v ->
+                List.indexedMap <|
+                    \xx v ->
                         if xx == x then
                             not v
+
                         else
                             v
-                    )
-        )
 
 
 genClicks : Int -> Int -> Random.Generator (List Click)
 genClicks w h =
     let
         s =
-            toFloat <| max w h
+            max w h |> toFloat
     in
-        Random.float (0.5 * s) (1.5 * s)
-            |> Random.andThen
-                (\n ->
-                    Random.list (truncate n) <|
-                        Random.map2 Click
-                            (Random.int 0 (w - 1))
-                            (Random.int 0 (h - 1))
-                )
+    Random.float (0.5 * s) (1.5 * s)
+        |> Random.andThen
+            (\n ->
+                Random.list (truncate n) <|
+                    Random.map2 Click
+                        (Random.int 0 (w - 1))
+                        (Random.int 0 (h - 1))
+            )
 
 
 gameIsOver : List2d -> Bool
